f08c536e7d582ba3f08fe345a8be65a80d4bf855
Johan Sebring <johan.sebring@gmail.com>
2016-01-05 17:00:25 +0100

    Tidy, no change in logic
    Moving some methods around to keep things organized

--- app/src/main/java/se/sebring/avgwhat/MainActivity.java
+++ app/src/main/java/se/sebring/avgwhat/MainActivity.java
@@ -31,6 +31,93 @@ public class MainActivity extends AppCompatActivity
     private SequenceManager mSequenceManager;
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        mPref = getSharedPreferences(PREF, MODE_PRIVATE);
+        //mPref.edit().clear().commit();
+        mSequenceManager = new SequenceManager(mPref);
+
+        SequenceListFrag frag = new SequenceListFrag();
+        getSupportFragmentManager().beginTransaction().add(R.id.content, frag, "FRAG").commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.nav_current:
+                Snackbar.make(getCurrentFocus(), "Current selected", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+                break;
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void loadCurrentFrag(Sequence s) {
+        Log.v(TAG, "loadCurrentFrag");
+
+        //Sequence s = sm.getCurrentSequence();
+        SequenceDetailFrag frag = SequenceDetailFrag.newInstance(s);
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, frag, "FRAG").addToBackStack(null).commit();
+    }
+
+    private void onFabClick(View view) {
+        Fragment frag = getSupportFragmentManager().findFragmentByTag("FRAG");
+        ((SequenceDetailFrag) frag).onFabClick(view);
+    }
+
+    @Override
     public List<Sequence> getAllActive() {
         return mSequenceManager.getAllActive();
     }
@@ -66,97 +153,9 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        mPref = getSharedPreferences(PREF, MODE_PRIVATE);
-        //mPref.edit().clear().commit();
-        mSequenceManager = new SequenceManager(mPref);
-
-        SequenceListFrag frag = new SequenceListFrag();
-        getSupportFragmentManager().beginTransaction().add(R.id.content, frag, "FRAG").commit();
-    }
-
-    private void loadCurrentFrag(Sequence s) {
-        Log.v(TAG, "loadCurrentFrag");
-
-        //Sequence s = sm.getCurrentSequence();
-        SequenceDetailFrag frag = SequenceDetailFrag.newInstance(s);
-        getSupportFragmentManager().beginTransaction().replace(R.id.content, frag, "FRAG").addToBackStack(null).commit();
-    }
-
-    private void onFabClick(View view) {
-        Fragment frag = getSupportFragmentManager().findFragmentByTag("FRAG");
-        ((SequenceDetailFrag) frag).onFabClick(view);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.nav_current:
-                Snackbar.make(getCurrentFocus(), "Current selected", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                break;
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
     public void onNewSequence(Sequence newSequence) {
         Log.v(TAG, "onNewSequence" + " - " + newSequence.getName());
         addSequence(newSequence);
         loadCurrentFrag(newSequence);
     }
-
 }
